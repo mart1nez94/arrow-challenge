0c79c580d0492022f8ac5d7ed95cf05b5c3823c4
mart1nez94 <armandoomtzz@gmail.com>
2020-03-12 17:56:10 -0600

    Adds 'show' method and route

--- app/controllers/arrows_controller.rb
+++ app/controllers/arrows_controller.rb
@@ -1,6 +1,11 @@
 class ArrowsController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @arrows = Arrow.by_user(current_user.id) if user_signed_in?
+  end
+
+  def show
+
   end
 end

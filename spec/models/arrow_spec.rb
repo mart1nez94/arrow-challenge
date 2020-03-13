792a8df8872ce7680d7f46421c057b7c73550e89
mart1nez94 <armandoomtzz@gmail.com>
2020-03-13 01:29:07 -0600

    Adds test cases for this feature

--- spec/models/arrow_spec.rb
+++ spec/models/arrow_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Arrow, type: :model do
+  before :all do
+    create_user(1, 'user@email.com', 'User', 'user123').save
+    create_user(2, 'another_user@email.com', 'Another User', 'user123').save
+  end
 
   context 'when need show all arrows' do
     it 'should return an array with the user arrows got' do
-      create_user(1, 'user@email.com', 'User', 'user123').save
-      create_user(2, 'another_user@email.com', 'Another User', 'user123').save
-  
       3.times do |time|
         create_arrow(time, "#{time} arrow", 2, 1).save
       end
@@ -19,15 +20,22 @@ RSpec.describe Arrow, type: :model do
 
   context 'when need show an arrow' do
     it 'should return a specific arrow' do
-      create_user(1, 'user@email.com', 'User', 'user123').save
-      create_user(2, 'another_user@email.com', 'Another User', 'user123').save
-
       create_arrow(1, 'Test', 2, 1).save
 
       arrow = Arrow.by_id(1, 1)
 
       expect(arrow.name).to eql 'Another User'
       expect(arrow.reason).to eql 'Test'
+    end
+  end
+
+  context 'when need create an arrow' do
+    it 'valid if is with attributes' do
+      expect(Arrow.new(reason: "reason", user_id: 2, to_user_id: 1)).to be_valid
+    end
+
+    it 'should return a response' do
+      expect(create_arrow(1, 'Test', 2, 1).save).to be_truthy 
     end
   end
 

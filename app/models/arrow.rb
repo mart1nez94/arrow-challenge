713465ec68106f25347acb8dc8b528f90936e0dd
mart1nez94 <armandoomtzz@gmail.com>
2020-03-20 00:00:48 -0600

    Changes the name of some functions

--- app/models/arrow.rb
+++ app/models/arrow.rb
@@ -1,16 +1,16 @@
 class Arrow < ApplicationRecord
   belongs_to :user
 
-  def self.get_by_user(user_id)
-    Arrow.all.where(to_user_id: user_id).order("arrows.created_at DESC")
+  def Arrow.get_by_user(user_id)
+    all.where(to_user_id: user_id).order("arrows.created_at DESC")
   end
 
-  def self.get_by_id(arrow_id, user_id)
-    Arrow.joins(:user).select("arrows.created_at, users.name, arrows.reason")
+  def Arrow.get_by_id(arrow_id, user_id)
+    joins(:user).select("arrows.created_at, users.name, arrows.reason")
       .find_by(arrows: { id: arrow_id, to_user_id: user_id })
   end
 
-  def self.insert(arrow_params)
-    Arrow.create!(arrow_params)
+  def Arrow.insert(arrow_params)
+    create!(arrow_params)
   end
 end
